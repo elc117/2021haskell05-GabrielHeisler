51b3975857e554b39170ec8b5911bd2c6ba4858b
Gabriel Heisler <gvheisler@inf.ufsm.br>
2021-06-07 15:58:37 -0300

    All done

--- haskell05.hs
+++ haskell05.hs
@@ -13,3 +13,23 @@ bmi' peso altura
   where
       imc = peso / altura ^ 2
 
+
+cpfValid :: [Int] -> Bool
+cpfValid cpf = dv1 == cpf !! 9 && dv2 == cpf !! 10
+  where digits = take 9 cpf
+        dv1 = cpfDV digits [10,9..]
+        dv2 = cpfDV (digits ++ [dv1]) [11,10..]
+ 
+
+cpfDV :: [Int] -> [Int] -> Int
+cpfDV digits mults = 
+    let expr = (sum $ zipWith (*) digits mults) `mod` 11
+    in if expr < 2 then 0 else 11-expr
+
+
+geraLista :: [(Bool, Bool)]
+geraLista = [(x,y) | x <- [False, True], y <- [False, True]]
+
+andTable :: [(Bool, Bool, Bool)]
+andTable = [(x, y, x && y) | (x, y) <- geraLista]
+
